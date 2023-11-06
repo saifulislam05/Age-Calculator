2a5d08a933266c24cb51105dd2199b6f574c61ab
saifulislam05 <saiful.bijulia@gmail.com>
2023-11-06 21:04:28 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
+
+
 const calculateBtn = document.getElementById("calculateBtn");
 
 const yearOutPut = document.getElementById("years");
 const monthOutPut = document.getElementById("months");
 const dayOutPut = document.getElementById("days");
 
+
 function calculateAge() {
-  const inputDate = document.getElementById("input").value;
+    const inputDate = document.getElementById("input").value;
   const selectedDate = new Date(inputDate);
   const currentDate = new Date();
 
+  // Check if selected date is greater than today's date
+  if (selectedDate > currentDate) {
+    alert("You cannot select a date greater than today's date.");
+    return; 
+  }
+
+  // Calculate the difference in years, months, and days
   let yearsDiff = currentDate.getFullYear() - selectedDate.getFullYear();
   let monthsDiff = currentDate.getMonth() - selectedDate.getMonth();
   let daysDiff = currentDate.getDate() - selectedDate.getDate();
 
+  // Handle cases where days or months are negative
   if (daysDiff < 0) {
     monthsDiff--;
     const lastDayOfMonth = new Date(
@@ -28,9 +39,12 @@ function calculateAge() {
     monthsDiff = 12 + monthsDiff;
   }
 
+  // Display the calculated age in the respective output elements
   yearOutPut.textContent = yearsDiff;
   monthOutPut.textContent = monthsDiff;
   dayOutPut.textContent = daysDiff;
+
+  // Show the output element
   const outPutElement = document.getElementById("outPut");
   outPutElement.style.display = "block";
 }
